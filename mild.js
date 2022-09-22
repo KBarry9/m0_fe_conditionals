5e9cb649855befe3d1518ae2b4e87d5b134be275
Kelly Barry <kleemuir@gmail.com>
2022-09-21 21:19:00 -0600

    Initial commit

---
+++ mild.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program that defines a variable that stores an Number. The program should print out the String "even" if the Number is even, and the String "odd" if the Number is odd.
+
+var temp = 62;
+
+if (temp %2 === 0) {
+  console.log("even")
+} else {
+  console.log("odd")
+}
